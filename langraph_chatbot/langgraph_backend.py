ddb27f44de064a3a42513ca8d5c74f9a7d25e5fa
Loblesh bhartal <lovelessbhartal12@gmail.com>
2026-01-27 19:51:05 +0545

    streaming feature

--- langraph_chatbot/langgraph_backend.py
+++ langraph_chatbot/langgraph_backend.py
@@ -1,10 +1,11 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
-from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
-from dotenv import load_dotenv
+
 
 from langchain_community.chat_models import ChatOllama
 
@@ -30,4 +31,5 @@ graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
-chatbot = graph.compile(checkpointer=checkpointer)
+chatbot = graph.compile(checkpointer=checkpointer) #this is old invoke without the streaming
+
