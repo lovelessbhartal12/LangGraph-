b897a46490427262e3a22046db25ad53212f5992
Loblesh bhartal <lovelessbhartal12@gmail.com>
2026-02-03 22:25:45 +0545

    short term memory in langgraph

--- langraph_chatbot/database_fronted.py
+++ langraph_chatbot/database_fronted.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from Backened_database import chatbot , retreive_all_thread 
-from langchain_core.messages import HumanMessage, AIMessage
+from database_backend import chatbot, retrieve_all_threads
+from langchain_core.messages import HumanMessage
 import uuid
 
-# utility functions 
+# **************************************** utility functions *************************
 
 def generate_thread_id():
     thread_id = uuid.uuid4()
@@ -21,11 +21,11 @@ def add_thread(thread_id):
 
 def load_conversation(thread_id):
     state = chatbot.get_state(config={'configurable': {'thread_id': thread_id}})
-   
+    # Check if messages key exists in state values, return empty list if not
     return state.values.get('messages', [])
 
 
-# Session Setup 
+# **************************************** Session Setup ******************************
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
@@ -33,12 +33,12 @@ if 'thread_id' not in st.session_state:
     st.session_state['thread_id'] = generate_thread_id()
 
 if 'chat_threads' not in st.session_state:
-    st.session_state['chat_threads'] = retreive_all_thread()
+    st.session_state['chat_threads'] = retrieve_all_threads()
 
 add_thread(st.session_state['thread_id'])
 
 
-#Sidebar UI
+# **************************************** Sidebar UI *********************************
 
 st.sidebar.title('LangGraph Chatbot')
 
@@ -64,9 +64,9 @@ for thread_id in st.session_state['chat_threads'][::-1]:
         st.session_state['message_history'] = temp_messages
 
 
-# Main UI 
+# **************************************** Main UI ************************************
 
-
+# loading the conversation history
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
@@ -80,20 +80,25 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    #CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
 
-     
-    with st.chat_message("assistant"):
-        def ai_only_stream():
-            for message_chunk, metadata in chatbot.stream(
-                {"messages": [HumanMessage(content=user_input)]},
-                config=CONFIG,
-                stream_mode="messages"
-            ):
-                if isinstance(message_chunk, AIMessage):
-                    # yield only assistant tokens
-                    yield message_chunk.content
+    CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {
+            "thread_id": st.session_state["thread_id"]
+        },
+        "run_name": "chat_turn",
+    }
 
-        ai_message = st.write_stream(ai_only_stream())
+    # first add the message to message_history
+    with st.chat_message('assistant'):
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= CONFIG,
+                stream_mode= 'messages'
+            )
+        )
 
     st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
